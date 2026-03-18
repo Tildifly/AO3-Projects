cd0471aa7e48b7ad9fe83f2574218b114b70cf4c
Tildifly <mathildeknuettel@gmail.com>
2026-03-18 22:55:43 +0100

    finished google app and history

--- interactive-phone/phone.js
+++ interactive-phone/phone.js
@@ -3,7 +3,7 @@ element.style.display = "block";   // zeigt als block (div-standard)
 element.style.display = "flex";    // zeigt als flexbox
 element.style.display = "grid";    // zeigt als grid
 */
-const screens = ["homescreen", "google"];
+const screens = ["homescreen", "google", "google-history"];
 
 function showScreen(id) {
   screens.forEach(function(s) {
@@ -12,20 +12,25 @@ function showScreen(id) {
   document.getElementById(id).style.display = "flex";
 }
 
+
 function activeHomescreen() {
   showScreen("homescreen");
-  document.getElementById("googleMenuPopup").style.display = "none";
 }
 
 function activeGoogle() {
   showScreen("google");
+  document.getElementById("googleMenuPopup").style.display = "none"; //so u cant see the popup immediately
 }
 
-function ActivegoogleMenuPopup() {
+function activegoogleMenuPopup() {
   let popup = document.getElementById("googleMenuPopup");
-  if (popup.style.display === "flex") {
+  if (popup.style.display === "flex") { //If-statement, so it dissapears if you click again
     popup.style.display = "none";
   } else {
     popup.style.display = "flex";
   }
+}
+
+function activeGoogleHistory(){
+    showScreen("google-history")
 }
